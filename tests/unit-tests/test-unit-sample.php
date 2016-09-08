f3da8c8dbcec2365d39fe7c2dc102a4ba1fd4844
Jonathan Bossenger <jonathanbossenger@gmail.com>
2016-09-08 11:52:58 +0200

    Adding to $get_option

--- tests/unit-tests/test-unit-sample.php
+++ tests/unit-tests/test-unit-sample.php
@@ -16,6 +16,9 @@ class UnitTests extends \PHPUnit_Framework_TestCase{
 
 	public function test_wcct_get_option() {
 	    $get_option = $this->getFunctionMock('WCCT', 'get_option');
+        $get_option>expects($this->once())
+            ->with($this->equalTo('wcct_venue'))
+            ->willReturn('river-club');
 	    $this->assertEquals( 'river-club', $this->wcct_get_option( 'venue' ) );
     }
 }
